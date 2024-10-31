563a6720cee73b316b23773117986a79dd8c2743
Lude Braz <Ludebraz@gmail.com>
2024-10-31 13:35:24 -0300

    entrega completa

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 /* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1,player2){
+
+    let resultado = " "
+
+    if(player1 == "Tesoura" && player2 == "Papel"){
+        resultado = "Jogador 1 venceu!"
+    } else if(player1 == "Pedra" && player2 == "Tesoura"){
+        resultado = "Jogador 1 venceu!"
+    }else if(player1 == "Papel" && player2 == "Pedra"){
+        resultado = "Jogador 1 venceu!"
+}else if(player1 == "Tesoura" && player2 == "Pedra"){
+    resultado = "Jogador 2 venceu!"
+} else if(player1 == "Pedra" && player2 == "Papel"){
+    resultado = "Jogador 2 venceu!"
+}else if(player1 == "Papel" && player2 == "Tesoura"){
+    resultado = "Jogador 2 venceu!"
+}else{resultado = "Empate!"}
+
+return resultado
+}
+
+console.log(playRockPaperScissor("Pedra", "Pedra"))
